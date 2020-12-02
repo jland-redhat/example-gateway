ad4bd00f6f4ebdd23180091123095f45f77aacd5
James Land <hokie10@gmail.com>
2020-12-02 15:15:34 -0500

    First commit

---
+++ src/main/java/com/redhat/todo/OpenAPI2SpringBoot.java
@@ -0,0 +1,65 @@
+package com.redhat.todo;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.redhat.todo", "com.redhat.todo.api" , "com.redhat.todo.config", "com.redhat.todo.repository"})
+public class OpenAPI2SpringBoot implements CommandLineRunner {
+
+    Logger log = LoggerFactory.getLogger(OpenAPI2SpringBoot.class);
+
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+            throw new ExitException();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(OpenAPI2SpringBoot.class).run(args);
+    }
+
+    static class ExitException extends RuntimeException implements ExitCodeGenerator {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int getExitCode() {
+            return 10;
+        }
+
+    }
+
+    @Bean
+    public WebMvcConfigurer webConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("Content-Type");
+            }
+        };
+    }
+
+    @Bean
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+}
---
+++ src/main/java/com/redhat/todo/api/TodosApiController.java
@@ -0,0 +1,52 @@
+package com.redhat.todo.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.swagger.annotations.ApiParam;
+
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+
+import com.redhat.todo.model.Todo;
+
+@Controller
+@RequestMapping("${openapi.todo.base-path:}")
+public class TodosApiController implements TodosApi {
+
+    @Autowired
+    RestTemplate rt;
+
+    @Value("${backendUrl}")
+    String backendUrl;
+
+    private final NativeWebRequest request;
+
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public TodosApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public ResponseEntity<List<Todo>> getTodos(@RequestParam(value = "completed", required = false) @Valid Boolean completed) {
+        return rt.exchange(backendUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<Todo>>(){} );
+    }
+}
